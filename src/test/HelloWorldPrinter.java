43cc41893bbbf9d80250cb899d9cb690fafc9109
ShimmerMan <jeremy.t.larsen@gmail.com>
2017-03-05 17:11:53 +1000

    Initial commit. Includes HelloWorld and a HelloWorldPrinter class that als prints Hello World.

---
+++ src/test/HelloWorldPrinter.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by Jeremy on 3/5/2017.
+ */
+public class HelloWorldPrinter {
+    public void printHello() {
+        System.out.println("Hello World from Class");
+    }
+}
---
+++ src/test/Main.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HelloWorldPrinter printer = new HelloWorldPrinter();
+
+	    System.out.println("Hello World");
+	    printer.printHello();
+    }
+}
